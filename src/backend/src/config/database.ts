c4134388270084511682dffff650097f1153387e
Rafaela Morais <155861986+rafafmorais@users.noreply.github.com>
2026-03-30 12:34:08 -0300

    versão atual db.ts

--- src/backend/src/config/database.ts
+++ src/backend/src/config/database.ts
@@ -1,15 +1,17 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  connectionString: "postgresql://postgres:Mayarpg2026@db.ppbvjaeuwbnchritpbeb.supabase.co:5432/postgres",
+  connectionString: "postgresql://postgres:Mayarpg2026@db.ppbvjaeuwbnchritpbeb.supabase.co:5432/postgres?sslmode=require",
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, 
   },
 });
+
+// Teste de conexão
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
-    console.error("Erro:", err);
+    console.error("Erro ao conectar no banco:", err);
   } else {
-    console.log("Conectado!", res.rows);
+    console.log("Conectado com sucesso!", res.rows);
   }
 });
