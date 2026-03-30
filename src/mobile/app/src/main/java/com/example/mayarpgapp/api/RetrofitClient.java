f2623f1664858c3955687f3dc0d26d9fed55ae45
Rafaela Morais <155861986+rafafmorais@users.noreply.github.com>
2026-03-30 12:21:42 -0300

    update retrofit com url do deploy

--- src/mobile/app/src/main/java/com/example/mayarpgapp/api/RetrofitClient.java
+++ src/mobile/app/src/main/java/com/example/mayarpgapp/api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.0.2.2:3001/api/";
+    private static final String BASE_URL = "https://maya-rpg-4r68.onrender.com/";
 
     private static Retrofit retrofit;
 
