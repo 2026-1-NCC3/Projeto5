6c4e0f8271c9e599c6557acb7991a316165716ec
Rafaela Morais <155861986+rafafmorais@users.noreply.github.com>
2026-04-01 19:29:27 -0300

    criando web folder

--- src/web/services/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "http://localhost:3000",
-}); //usado para fazer as requisições para o backend, a porta deve ser a mesma do backend, no caso 3000
