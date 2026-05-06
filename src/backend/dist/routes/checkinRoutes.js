bfaf89a483552624daabc38f0442ed5f8cf6e005
Rafaela Morais <155861986+rafafmorais@users.noreply.github.com>
2026-05-06 12:36:05 -0300

    add gitginore no backend

--- src/backend/dist/routes/checkinRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const checkinController_1 = require("../controllers/checkinController");
-const authMiddleware_1 = require("../midllewares/authMiddleware");
-const router = (0, express_1.Router)();
-router.post("/checkin", authMiddleware_1.authenticateToken, checkinController_1.fazerCheckin);
-router.get("/checkin/historico", authMiddleware_1.authenticateToken, checkinController_1.historicoCheckins);
-exports.default = router;
--- src/backend/dist/routes/exerciseRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const exerciseController_1 = require("../controllers/exerciseController");
-const router = (0, express_1.Router)();
-router.get("/exercises", exerciseController_1.listExercises);
-router.post("/exercises", exerciseController_1.addExercise);
-exports.default = router;
